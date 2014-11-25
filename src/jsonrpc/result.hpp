853b53b2720f2403297ddb48013d836bf646f9bb
James Goode <james@uwcs.co.uk>
2014-11-25 15:29:24 +0000

    Conform slightly to the JSONRPC2 standard.

--- src/jsonrpc/result.hpp
+++ src/jsonrpc/result.hpp
@@ -13,10 +13,18 @@ namespace atlas
          */
         struct result : public styx::object_accessor
         {
+            /*!
+             * \brief Create a result with the JSONRPC version preset.
+             */
             result()
             {
+                jsonrpc() = "2.0";
             }
 
+            /*!
+             * \brief Create a result from an existing JSON element.  The
+             * JSONRPC version attribute will not be set.
+             */
             explicit result(styx::element& e) :
                 styx::object_accessor(e)
             {
@@ -35,6 +43,18 @@ namespace atlas
              * authentiation error.
              */
             bool& unauthorised() { return get_bool("unauthorised"); }
+            /*!
+             * \brief JSONRPC allows the client to attach an id to each
+             * procedure call.  The id can be of any type and should be copied
+             * to the response.
+             */
+            styx::element& id() { return get_element("id"); }
+
+            /*!
+             * \brief The version of JSONRPC understood by the server should be
+             * included in the response.
+             */
+            std::string& jsonrpc() { return get_string("jsonrpc"); }
         };
     }
 }
