0719655ec7889d1a8535d40055dbda32ef8669e7
James Goode <james@uwcs.co.uk>
2015-05-07 20:49:39 +0100

    Remove target architecture from objcopy commands.

--- cmake/atlas_add_objcopy_target.cmake
+++ cmake/atlas_add_objcopy_target.cmake
@@ -1,7 +1,9 @@
 macro(atlas_add_objcopy_target prefix filename object)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${object}
-        COMMAND objcopy --prefix-symbols=${prefix} --input binary --output elf64-x86-64 --binary-architecture i386 ${filename} ${CMAKE_CURRENT_BINARY_DIR}/${object}
+        COMMAND ld -r -o ${CMAKE_CURRENT_BINARY_DIR}/${object} -z noexecstack --format=binary ${filename}
+        COMMAND objcopy --prefix-symbols=${prefix} --rename-section .data=.rodata,alloc,load,readonly,data,contents ${CMAKE_CURRENT_BINARY_DIR}/${object}
+        #COMMAND objcopy --prefix-symbols=${prefix} --input binary ${filename} ${CMAKE_CURRENT_BINARY_DIR}/${object}
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${filename}
         COMMENT "Generating ${object} from ${filename}" VERBATIM
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
